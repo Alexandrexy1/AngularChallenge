4e88e2725e9f222f639921da8b9398db759a577e
Alexandre <alexandrenas2003@gmail.com>
2024-12-19 16:10:16 -0300

    two way data binding and ngIf

---
+++ challenge-3/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'challenge-3';
+  name: string = "Nelipe";
+
+  isGreenyellow: boolean = true;
+}
