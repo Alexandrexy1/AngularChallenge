f78237feed730ff8434aff7746bd2522aa29fd40
Alexandre <alexandrenas2003@gmail.com>
2024-12-25 19:52:25 -0300

    ngStyle and ngClass

--- challenge-3/src/app/app.component.ts
+++ challenge-3/src/app/app.component.ts
@@ -7,29 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'challenge-3';
-  selectedPersonIndex: number | undefined;
+  isRed: boolean = true;
 
-  peopleList = [
-    {
-      name: "Matheus Arruda",
-      age: 23
-    },
-    {
-      name: "Maria Alice",
-      age: 19
-    },
-    {
-      name: "Alexandre Nascimento",
-      age: 21
-    },
-    {
-      name: "Jeniffer Souza",
-      age: 18
-    }
-  ]
 
-  selectedPerson(index: number) {
-    this.selectedPersonIndex = index;
+  changeH1Class() {
+    this.isRed = !this.isRed;
   }
 }
